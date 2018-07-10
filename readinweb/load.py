405fa6c6ff5d324f28fc39d483f8abf2ed22d444
Dustin Lang <dstndstn@gmail.com>
2018-07-10 12:26:40 -0400

    csv reader: delimiter typo; print rows as they are read

--- readinweb/load.py
+++ readinweb/load.py
@@ -13,9 +13,10 @@ import fitsio
 #T.cut(np.argsort(T.coadd_id))
 
 csv_filepathname="centrals.csv"
-dataReader = csv.reader(open(csv_filepathname), delimeter=',', quotechar='"')
+dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
 
 for row in dataReader:
+    print('Row:', row)
     halos=Halos()
     halos.name=row[0]
     halos.ra = row[1]
