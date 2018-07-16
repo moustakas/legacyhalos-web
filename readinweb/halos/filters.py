62f5ff09cc72a5ef097ba792925b93c2eea3c01d
desi1 <desi1@nyx>
2018-07-16 16:00:03 -0400

    More updates

--- readinweb/halos/filters.py
+++ readinweb/halos/filters.py
@@ -5,18 +5,18 @@ from readinweb.models import Centrals
 
 class CentralsFilter(django_filters.FilterSet):
     #ra = django_filters.NumberFilter()
-    ra__gt = django_filters.NumberFilter(name='ra', lookup_expr='gt')
+    ra__gt = django_filters.NumberFilter(name='ra', lookup_expr='gt', label='Custom')
     ra__lt = django_filters.NumberFilter(name='ra', lookup_expr='lt')
 
     #dec = django_filters.NumberFilter()
     dec__gt = django_filters.NumberFilter(name='dec', lookup_expr='gt')
     dec__lt = django_filters.NumberFilter(name='dec', lookup_expr='lt')
 
+    #For regular titles
+    mem_match_id = django_filters.CharFilter(label='redMaPPer ID')
     class Meta:
         model = Centrals
         fields = ['mem_match_id']
-
-## class CentralsFilter(django_filters.FilterSet):
     
 
 ##     class Meta:
