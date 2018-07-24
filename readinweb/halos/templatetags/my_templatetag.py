b9ae483acf2149441ae69900aed82b591cc85901
desi1 <desi1@nyx>
2018-07-24 11:36:11 -0400

    Sessions funtional, and loging information on search

--- readinweb/halos/templatetags/my_templatetag.py
+++ readinweb/halos/templatetags/my_templatetag.py
@@ -2,8 +2,46 @@ from django import template
 
 register = template.Library()
 
+#try another decorator?
 @register.simple_tag
 def url_replace(req, field, value):
     dict_ = req.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
+
+@register.simple_tag
+def url_pull(req):
+    dict_ = req.GET.copy()
+    search = "Search Criteria:"
+    entry = False
+    if "mem_match_id" in dict_ and dict_["mem_match_id"] is not "":
+        search += " redMaPPer ID: " + dict_["mem_match_id"] + " |"
+        entry = True
+    if "ra__gt" in dict_ and dict_["ra__gt"] is not "":
+        search += " RA low: " + dict_["ra__gt"] + " |"
+        entry = True
+    if "ra__lt" in dict_ and dict_["ra__lt"] is not "":
+        search += " RA high: " + dict_["ra__lt"] + " |"
+        entry = True
+    if "dec__gt" in dict_ and dict_["dec__gt"] is not "":
+        search += " Dec low: " + dict_["dec__gt"] + " |"
+        entry = True
+    if "dec__lt" in dict_ and dict_["dec__lt"] is not "":
+        search += " Dec high: " + dict_["dec__lt"] + " |"
+        entry = True
+    if not entry:
+        search = "Showing all results"
+    else:
+        search = search[:-1]
+    return search
+
+@register.simple_tag
+def viewer_link(ra, dec):
+    baseurl = 'http://legacysurvey.org/viewer/'
+    viewer = '{}?ra={:.6f}&dec={:.6f}&zoom=15&layer=decals-dr5'.format(
+        baseurl, ra, dec)
+    return viewer
+
+@register.simple_tag
+def skyserver_link(sdss_objid):
+        return 'http://skyserver.sdss.org/dr14/en/tools/explore/summary.aspx?id=%d' % sdss_objid
--- readinweb/halos/views.py
+++ readinweb/halos/views.py
@@ -3,27 +3,40 @@ from django.shortcuts import render
 from readinweb.models import Centrals
 from .filters import CentralsFilter
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.serializers import serialize
+import json
+from django.http import HttpResponse 
 
 
 def list(req):
+    
     cen_filter = CentralsFilter(req.GET, queryset=Centrals.objects.all().order_by('mem_match_id'))
     cen_filtered = cen_filter.qs
-    
-    #put each item in the list in a loop
-    req.session['results_list'] = list(Centrals.objects.filter(cen_filtered))
+    req.session['results_list'] = serialize('json', cen_filtered, cls=LazyEncoder)
     paginator = Paginator(cen_filtered, 50)
     page_num = req.GET.get('page')
     page = paginator.get_page(page_num)
-    return render(req, 'list.html', {'page': page})
+    return render(req, 'list.html', {'page': page, 'paginator': paginator})
 
 def index(req):
     return render(req, 'index.html')
 
 def centrals(req):
-    index = req.GET.get('index')
-    cen_list = request.session['results_list']
-    current_cen = cen_list[index - 1]
-    #prev_cen = cen_list[index - 2]
-    #next_cen = cen_list[index]
-    return render(req, 'centrals.html', {'current_cen': current_cen, 'index': index}) 
+    index = int(req.GET.get('index'))
+    cen_list = json.loads(req.session['results_list'])
+    cen = cen_list[index - 1]['fields']
+    prev_index = index - 1
+    if (prev_index <= 0):
+        prev_index = len(cen_list)
+    next_index = index + 1
+    if (next_index >= len(cen_list)):
+        next_index = 1
+    return render(req, 'centrals.html', {'cen_list': cen_list, 'index': index, 'cen': cen, 'next_index': next_index, 'prev_index': prev_index}) 
+
+class LazyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if instance(obj, Centrals):
+            return str(obj)
+        return super().default(obj)
+        
