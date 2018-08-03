1b4529167bd76d2e9b5b8c9c5b805798583df975
John Moustakas <jmoustakas@siena.edu>
2018-08-03 07:40:20 -0400

    change name to field_name in django.forms.filters

--- centrals/filters.py
+++ centrals/filters.py
@@ -4,20 +4,20 @@ from legacyhalos_web.models import Centrals
 class CentralsFilter(django_filters.FilterSet):
     #name is the Centrals object variable
     #lookup_expr is used to get ranges (currently using greater/less than or equal to  
-    ra__gte = django_filters.NumberFilter(name='ra', lookup_expr='gte')
-    ra__lte = django_filters.NumberFilter(name='ra', lookup_expr='lte')
+    ra__gte = django_filters.NumberFilter(field_name='ra', lookup_expr='gte')
+    ra__lte = django_filters.NumberFilter(field_name='ra', lookup_expr='lte')
 
-    dec__gte = django_filters.NumberFilter(name='dec', lookup_expr='gte')
-    dec__lte = django_filters.NumberFilter(name='dec', lookup_expr='lte')
+    dec__gte = django_filters.NumberFilter(field_name='dec', lookup_expr='gte')
+    dec__lte = django_filters.NumberFilter(field_name='dec', lookup_expr='lte')
 
-    z__gte = django_filters.NumberFilter(name='z', lookup_expr='gte')
-    z__lte = django_filters.NumberFilter(name='z', lookup_expr='lte')
+    z__gte = django_filters.NumberFilter(field_name='z', lookup_expr='gte')
+    z__lte = django_filters.NumberFilter(field_name='z', lookup_expr='lte')
 
-    la__gte = django_filters.NumberFilter(name='la', lookup_expr='gte')
-    la__lte = django_filters.NumberFilter(name='la', lookup_expr='lte')
+    la__gte = django_filters.NumberFilter(field_name='la', lookup_expr='gte')
+    la__lte = django_filters.NumberFilter(field_name='la', lookup_expr='lte')
 
-    mem_match_id__gte = django_filters.NumberFilter(name='mem_match_id', lookup_expr='gte')
-    mem_match_id__lte = django_filters.NumberFilter(name='mem_match_id', lookup_expr='lte')
+    mem_match_id__gte = django_filters.NumberFilter(field_name='mem_match_id', lookup_expr='gte')
+    mem_match_id__lte = django_filters.NumberFilter(field_name='mem_match_id', lookup_expr='lte')
 
     class Meta:
         model = Centrals
