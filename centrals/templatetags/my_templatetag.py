a413fec6f60be14c322061d7bb59faafdfe20a0b
Alissa Ronca <31697397+aronca@users.noreply.github.com>
2018-08-07 15:59:47 -0400

    Update my_templatetag.py

--- centrals/templatetags/my_templatetag.py
+++ centrals/templatetags/my_templatetag.py
@@ -101,13 +101,6 @@ def url_pull(req):
         search = search[:-1]
     return search
 
-def create_temp(**kwargs):
-    tempdir = os.environ.get('TMPDIR', os.environ.get('TMP','/tmp'))
-    f, fn = tempfile.mkstemp(dir=tempdir, **kwargs)
-    os.close(f)
-    os.unlink(fn)
-    return fn
-
 @register.simple_tag
 def photo_pull(req, id_num, img_name):
     path = "static/data/" + id_num + "/" + id_num + "-" + img_name 
@@ -121,65 +114,4 @@ def viewer_link(ra, dec):
 
 @register.simple_tag
 def skyserver_link(sdss_objid):
-        return 'http://skyserver.sdss.org/dr14/en/tools/explore/summary.aspx?id=%d' % sdss_objid
-
-@register.simple_tag
-def download(req):
-    #cen_list = pickle.loads(req.session['results_list'])
-    #for cen in cen_list:
-    #    table
-    # Create your table of results... probably from a database query.
-    # Should probably use astropy.io.fits instead of astrometry.util.fits!
-    #data_rows = [{1,2.0, 'x'},
-    #             {4,5.0, 'y'},
-    #             {5,8.2, 'z'}]
-    #table = Table(rows=data_rows, names=('a','b','c'))
-    #table.write
-    #table = fits_table()
-    table = Table([[1, 2], [4, 5], [7, 8]], names=('a', 'b', 'c'))
-    #tempfiletable =  tempfile.mkstemp(suffix='.fits')
-    #table.write(tempfiletable, format='fits')
-    #table.x = np.arange(3)
-    f,tmpfn = tempfile.mkstemp(suffix='.fits')
-    os.close(f)
-    os.unlink(tmpfn)
-    # Write the FITS file contents...
-    table.write(tmpfn)
-    return send_file(tmpfn, 'image/fits', unlink=True, filename='results.fits')
-
-
-def send_file(fn, content_type, unlink=False, modsince=None, expires=3600,
-              filename=None):
-    import datetime
-    from django.http import HttpResponseNotModified, StreamingHttpResponse
-    '''
-    modsince: If-Modified-Since header string from the client.
-    '''
-    st = os.stat(fn)
-    f = open(fn, 'rb')
-    if unlink:
-        os.unlink(fn)
-    # file was last modified...
-    lastmod = datetime.datetime.fromtimestamp(st.st_mtime)
-
-    if modsince:
-        #print('If-modified-since:', modsince #Sat, 22 Nov 2014 01:12:39 GMT)
-        ifmod = datetime.datetime.strptime(modsince, '%a, %d %b %Y %H:%M:%S %Z')
-        #print('Parsed:', ifmod)
-        #print('Last mod:', lastmod)
-        dt = (lastmod - ifmod).total_seconds()
-        if dt < 1:
-            return HttpResponseNotModified()
-
-    res = StreamingHttpResponse(f, content_type=content_type)
-    # res['Cache-Control'] = 'public, max-age=31536000'
-    res['Content-Length'] = st.st_size
-    if filename is not None:
-        res['Content-Disposition'] = 'attachment; filename="%s"' % filename
-    # expires in an hour?
-    now = datetime.datetime.utcnow()
-    then = now + datetime.timedelta(0, expires, 0)
-    timefmt = '%a, %d %b %Y %H:%M:%S GMT'
-    res['Expires'] = then.strftime(timefmt)
-    res['Last-Modified'] = lastmod.strftime(timefmt)
-    return res
+    return 'http://skyserver.sdss.org/dr14/en/tools/explore/summary.aspx?id=%d' % sdss_objid
