432b86a4aa8832e1fa14d44482c9bd8761784022
Alissa Ronca <31697397+aronca@users.noreply.github.com>
2018-08-10 15:35:41 -0400

    Update filters.py

--- centrals/filters.py
+++ centrals/filters.py
@@ -1,15 +1,17 @@
 """
-This is a custom filter for the Centrals model, 
-which selects Centrals objects in the database based on meeting desired criteria.
-
-Filter options include greater than or equal to, and less than or equal to 
-on the following Centrals fields: ra, dec, z, and la.
-The filter can be used in a form on our webpage, as seen in list.html. 
+This holds custom filters for the Centrals model, 
+which work by selecting Centrals objects in the database based on meeting the desired criteria.
 """
 import django_filters
 from legacyhalos_web.models import Centrals
 
 class CentralsFilter(django_filters.FilterSet):
+    """
+    Our custom filter for the centrals model.
+    Filter options include greater than or equal to, and less than or equal to 
+    on the following Centrals fields: ra, dec, z, and la.
+    The filter can be used in a form on our webpage, as seen in list.html. 
+    """
     #field_name is the Centrals object variable
     #lookup_expr is used to get ranges (currently using greater/less than or equal to  
     ra__gte = django_filters.NumberFilter(field_name='ra', lookup_expr='gte')
@@ -29,7 +31,7 @@ class CentralsFilter(django_filters.FilterSet):
 
     class Meta:
         model = Centrals
-        #add variable to the field if looking for exact match
+        #add variable to fields[] if looking for exact match
         fields = []
 
         def id(self):
